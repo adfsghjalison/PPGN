90ae24e753a3f8131be7dd5cd5bfff84336e00d7
adfsghjalison <adfsghjalison@gmail.com>
2018-12-17 01:27:45 +0800

    correct some description of arguments - g, l2

--- flags.py
+++ flags.py
@@ -6,25 +6,27 @@ tf.app.flags.DEFINE_string('mode','test', 'test / stdin')
 # persona-dialogue
 tf.app.flags.DEFINE_string('model_dir', '../VRAE/model', 'output_model_dir') 
 tf.app.flags.DEFINE_string('data_dir', '../VRAE/data', 'data dir')
-tf.app.flags.DEFINE_string('data_name','NLPCC', 'data dir')
+#tf.app.flags.DEFINE_string('data_name','NLPCC', 'data dir')
+tf.app.flags.DEFINE_string('data_name','BG', 'data dir')
 tf.app.flags.DEFINE_string('KL_annealing', True, 'whether do KL annealing')
 tf.app.flags.DEFINE_float('word_dp', 0.3, 'word dropout rate')
-tf.app.flags.DEFINE_integer('sequence_length', 15, 'sentence length')
+#tf.app.flags.DEFINE_integer('sequence_length', 15, 'sentence length')
+tf.app.flags.DEFINE_integer('sequence_length', 10, 'sentence length')
 tf.app.flags.DEFINE_integer('batch_size', 40, 'batch size')
 tf.app.flags.DEFINE_integer('latent_dim', 500, 'latent_size')
 
 # sentiment-classifier
-tf.app.flags.DEFINE_string('senti_model_dir', '../sentiment/model', 'sentiment model dir')
+tf.app.flags.DEFINE_string('senti_model_dir', '../style/model', 'sentiment model dir')
 tf.app.flags.DEFINE_integer('embedding_dim', 300, 'embedding_dim')
 tf.app.flags.DEFINE_integer('unit_size', 256, 'unit_size')
 #tf.app.flags.DEFINE_integer('max_length', 15, 'max_length')
 
 tf.app.flags.DEFINE_string('soft', False, 'whether decode soft')
 tf.app.flags.DEFINE_string('out', 'output', 'testing output file')
-tf.app.flags.DEFINE_boolean('pos', True, 'positive or negative')
+tf.app.flags.DEFINE_boolean('pos', False, 'positive or negative')
 
-tf.app.flags.DEFINE_integer('g', 300, 'word dropout rate')
-tf.app.flags.DEFINE_integer('l2', 50, 'word dropout rate')
+tf.app.flags.DEFINE_integer('g', 400, 'style gradient weight')
+tf.app.flags.DEFINE_integer('l2', 25, 'L2 norm gradient weight')
 
 FLAGS = tf.app.flags.FLAGS
 
